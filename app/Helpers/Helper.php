33a293ea1f42fd2b2f260844ec269024a9ebae32
gai871013 <wang.gaichao@163.com>
2017-07-24 18:20:07 +0800

    2017.7.24.1

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -490,4 +490,21 @@ class Helper
         return $client->get($url, ['query' => $params])->getBody()->getContents();
     }
 
+    public static function post($url, $data)
+    {
+        $postdata = http_build_query(
+            $data
+        );
+
+        $opts = array('http' =>
+            array(
+                'method' => 'POST',
+                'header' => 'Content-type: application/x-www-form-urlencoded',
+                'content' => $postdata
+            )
+        );
+        $context = stream_context_create($opts);
+        $result = file_get_contents($url, false, $context);
+        return $result;
+    }
 }
