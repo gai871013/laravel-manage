d13e030e30ef35f6ebb48ec77d37e3396f5184c1
gai871013 <wang.gaichao@163.com>
2017-07-28 02:26:12 +0800

    2017.7.28.3 admin_actions

--- database/migrations/2017_04_26_075926_create_admin_actions_table.php
+++ database/migrations/2017_04_26_075926_create_admin_actions_table.php
@@ -20,7 +20,7 @@ class CreateAdminActionsTable extends Migration
             $table->string('code', 100)->default('')->comment('代表权限的英文字符串，对应汉文在语言文件中，如果该字段有某个字符串，就表示有该权限');
             $table->string('lang', 20)->default('')->comment('权限名称');
             $table->string('route', 30)->default('')->nullable()->comment('目录名');
-            $table->string('param', 30)->default('')->comment('参数');
+            $table->string('param', 30)->default('')->nullable()->comment('参数');
             $table->integer('enable')->default(1)->comment('是否显示/使用');
             $table->string('remark', 50)->default('')->comment('备注');
             $table->string('icon', 30)->default('')->comment('图标');
