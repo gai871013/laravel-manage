0dfc05857cccd6343fe3cfcdd391b2a373bdd42f
gai871013 <wang.gaichao@163.com>
2017-07-28 02:09:20 +0800

    2017.7.28.1 fix admin_actions route column can not null

--- database/migrations/2017_04_26_075926_create_admin_actions_table.php
+++ database/migrations/2017_04_26_075926_create_admin_actions_table.php
@@ -19,7 +19,7 @@ class CreateAdminActionsTable extends Migration
             $table->integer('parent_id')->default(0)->comment('该id项的父id，对应本表的id字段');
             $table->string('code', 100)->default('')->comment('代表权限的英文字符串，对应汉文在语言文件中，如果该字段有某个字符串，就表示有该权限');
             $table->string('lang', 20)->default('')->comment('权限名称');
-            $table->string('route', 30)->default('')->comment('目录名');
+            $table->string('route', 30)->default('')->nullable()->comment('目录名');
             $table->string('param', 30)->default('')->comment('参数');
             $table->integer('enable')->default(1)->comment('是否显示/使用');
             $table->string('remark', 50)->default('')->comment('备注');
