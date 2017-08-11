0d4280b1ea6ee519287b9658110c0ad7f389b2bf
gai871013 <wang.gaichao@163.com>
2017-08-11 16:35:07 +0800

    2017.8.11.2 修复menuEdit路径undefined错误

--- resources/views/tree/menuSelect.blade.php
+++ resources/views/tree/menuSelect.blade.php
@@ -1,15 +1,15 @@
 @php
-    $id = isset($id) ? $id : 0;
-    $deep = isset($deep) ? (int)$deep : 1;
-    $url = isset($url) ? $url : url(config('app.admin_path')) . '/';
+	$id = isset($id) ? $id : 0;
+	$deep = isset($deep) ? (int)$deep : 1;
+	$url = isset($url) ? $url : url(config('app.admin_path')) . '/';
 @endphp
 @foreach($menu as $v)
-    <option data-url="{{ $url.$v['code'] }}" @if($id == $v['id']) selected
-            @endif value="{{ $v['id'] }}">@for($i = 0; $i < $deep; $i++)&nbsp;&nbsp;&nbsp;
-        &nbsp;@endfor {{ empty($v['remark']) ? trans('admin.' . $v['lang']) : $v['remark'] }}</option>
-    @if(isset($v['children']))
-        @php($deep++)
-        @include('tree.menuSelect', ['menu' => $v['children'], 'id'=> $id, 'deep' => $deep, 'url' => $url . $v['code'] . '/'])
-        @php($deep--)
-    @endif
+	<option data-url="{{ $url . $v['code'] . '/' }}" @if($id == $v['id']) selected
+	        @endif value="{{ $v['id'] }}">@for($i = 0; $i < $deep; $i++)&nbsp;&nbsp;&nbsp;
+		&nbsp;@endfor {{ empty($v['remark']) ? trans('admin.' . $v['lang']) : $v['remark'] }}</option>
+	@if(isset($v['children']))
+		@php($deep++)@endphp
+		@include('tree.menuSelect', ['menu' => $v['children'], 'id'=> $id, 'deep' => $deep, 'url' => $url . $v['code'] . '/'])
+		@php($deep--)@endphp
+	@endif
 @endforeach
