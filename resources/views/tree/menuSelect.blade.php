85675e7fe26bca671143d8f55305f81040b69b77
王朝 <wang.gaichao@163.com>
2018-03-23 15:10:16 +0800

    2018.03.23.2 优化添加菜单选择框内显示效果

--- resources/views/tree/menuSelect.blade.php
+++ resources/views/tree/menuSelect.blade.php
@@ -2,14 +2,20 @@
 	$id = isset($id) ? $id : 0;
 	$deep = isset($deep) ? (int)$deep : 1;
 	$url = isset($url) ? $url : url(config('app.admin_path')) . '/';
+	$ii = 1;
 @endphp
 @foreach($menu as $v)
 	<option data-url="{{ $url . $v['code'] . '/' }}" @if($id == $v['id']) selected
-	        @endif value="{{ $v['id'] }}">@for($i = 0; $i < $deep; $i++)&nbsp;&nbsp;&nbsp;
+	        @endif value="{{ $v['id'] }}">@for($i = 0; $i < $deep; $i++)&nbsp;&nbsp;&nbsp;@if(count($menu) == $ii)└─@else├─@endif
 		&nbsp;@endfor {{ empty($v['remark']) ? trans('admin.' . $v['lang']) : $v['remark'] }}</option>
 	@if(isset($v['children']))
-		@php($deep++)@endphp
+		@php
+			$deep++;
+		@endphp
 		@include('tree.menuSelect', ['menu' => $v['children'], 'id'=> $id, 'deep' => $deep, 'url' => $url . $v['code'] . '/'])
-		@php($deep--)@endphp
+		@php
+			$deep--;
+			$ii++;
+		@endphp
 	@endif
 @endforeach
