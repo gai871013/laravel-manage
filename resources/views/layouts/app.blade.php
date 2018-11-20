552988af4d2baaaa54eaef56f333b307335a1e48
gai871013 <wang.gaichao@163.com>
2018-11-20 12:17:36 +0800

    2018.11.20.6 天气

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,100 +1,101 @@
 <!DOCTYPE html>
 <html lang="{{ config('app.locale') }}">
 <head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<!-- CSRF Token -->
-	<meta name="csrf-token" content="{{ csrf_token() }}">
-	<title>@yield('title') - {{ config('app.name', 'LaravelManageSystem') }}</title>
-	<meta name="author" content="gai871013">
-	<meta name="keywords" content="{{ config('app.name', 'LaravelManageSystem') }}@yield('keywords')">
-	<meta name="description" content="@yield('description')">
-	<!-- Styles -->
-	<link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
-	<link rel="stylesheet" href="{{ asset('css/style.css') }}">
-	<link rel="stylesheet" href="{{ asset('highlight/monokai-sublime.css') }}">
-	<link href="{{ asset(mix('css/app.css')) }}" rel="stylesheet">
-	<script src="{{ asset(mix('js/app.js')) }}"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.lazyload/1.9.1/jquery.lazyload.min.js"></script>
-	<script src="{{ asset('highlight/highlight.pack.js') }}"></script>
-	<script>hljs.initHighlightingOnLoad();</script>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>@yield('title') - {{ config('app.name', 'LaravelManageSystem') }}</title>
+    <meta name="author" content="gai871013">
+    <meta name="keywords" content="{{ config('app.name', 'LaravelManageSystem') }}@yield('keywords')">
+    <meta name="description" content="@yield('description')">
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('highlight/monokai-sublime.css') }}">
+    <link href="{{ asset(mix('css/app.css')) }}" rel="stylesheet">
+    <script src="{{ asset(mix('js/app.js')) }}"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.lazyload/1.9.1/jquery.lazyload.min.js"></script>
+    <script src="{{ asset('highlight/highlight.pack.js') }}"></script>
+    <script>hljs.initHighlightingOnLoad();</script>
 
-	<!--[if gte IE 9]>
-	<script src="{{ asset('js/jquery-1.11.1.min.js') }}"></script>
-	<script src="{{ asset('js/html5shiv.min.js')  }}"></script>
-	<script src="{{ asset('js/respond.min.js') }}"></script>
+    <!--[if gte IE 9]>
+    <script src="{{ asset('js/jquery-1.11.1.min.js') }}"></script>
+    <script src="{{ asset('js/html5shiv.min.js')  }}"></script>
+    <script src="{{ asset('js/respond.min.js') }}"></script>
 
-	<![endif]-->
-	<!--[if lt IE 9]>
-	<script>window.location.href = 'upgrade-browser';</script>
-	<![endif]-->
+    <![endif]-->
+    <!--[if lt IE 9]>
+    <script>window.location.href = 'upgrade-browser';</script>
+    <![endif]-->
 
-	@yield('head')
+    @yield('head')
 </head>
 <body>
 <header class="header">
-	<nav class="navbar navbar-default" id="navbar">
-		<div class="container">
-			<div class="header-topbar hidden-xs link-border">
-				{{--<ul class="site-nav topmenu">
+    <nav class="navbar navbar-default" id="navbar">
+        <div class="container">
+            <div class="header-topbar hidden-xs link-border">
+                {{--<ul class="site-nav topmenu">
 
-					<li><a href="/links" rel="nofollow">友情链接</a></li>
-					<li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" role="button"
-					                        aria-haspopup="true" aria-expanded="false" rel="nofollow">关注本站 <span
-									class="caret"></span></a>
-						<ul class="dropdown-menu header-topbar-dropdown-menu">
-							<li><a data-toggle="modal" data-target="#WeChat" rel="nofollow"><i class="fa fa-weixin"></i>
-									微信</a></li>
-							<li><a href="http://weibo.com/5742755940" target="_blank" rel="nofollow"><i
-											class="fa fa-weibo"></i> 微博</a></li>
-							<li><a data-toggle="modal" data-target="#qq" rel="nofollow"><i class="fa fa-qq"></i>交流群</a>
-							</li>
-						</ul>
-					</li>
-				</ul>--}}
+                    <li><a href="/links" rel="nofollow">友情链接</a></li>
+                    <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" role="button"
+                                            aria-haspopup="true" aria-expanded="false" rel="nofollow">关注本站 <span
+                                    class="caret"></span></a>
+                        <ul class="dropdown-menu header-topbar-dropdown-menu">
+                            <li><a data-toggle="modal" data-target="#WeChat" rel="nofollow"><i class="fa fa-weixin"></i>
+                                    微信</a></li>
+                            <li><a href="http://weibo.com/5742755940" target="_blank" rel="nofollow"><i
+                                            class="fa fa-weibo"></i> 微博</a></li>
+                            <li><a data-toggle="modal" data-target="#qq" rel="nofollow"><i class="fa fa-qq"></i>交流群</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>--}}
 
-				{{--<a data-toggle="modal" data-target="#loginModal" class="login" rel="nofollow">Hi,请登录</a>&nbsp;&nbsp;--}}
-				{{--<a data-toggle="modal" data-target="#regModal" class="register" rel="nofollow">我要注册</a>--}}
-			</div>
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
-				        data-target="#header-navbar" aria-expanded="false"><span class="sr-only"></span> <span
-							class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span>
-				</button>
-				<h1 class="logo hvr-bounce-in" style="margin-top:10px;"><a href="{{ route('index') }}" title="">{{ config('app.name') }}</a></h1>
-			</div>
-			<div class="collapse navbar-collapse" id="header-navbar">
-				<ul class="nav navbar-nav navbar-right">
-					<li><a data-cont="{{ config('app.name') }}" href="{{ route('index') }}">@lang('index.index')</a>
-					</li>
-					@if(isset($nav))
-						@foreach($nav as $v)
-							<li><a href="{{ route('category', ['id' => $v->id]) }}">{{ $v->catname }}</a></li>
-						@endforeach
-					@endif
-				</ul>
-			</div>
-		</div>
-	</nav>
+                {{--<a data-toggle="modal" data-target="#loginModal" class="login" rel="nofollow">Hi,请登录</a>&nbsp;&nbsp;--}}
+                {{--<a data-toggle="modal" data-target="#regModal" class="register" rel="nofollow">我要注册</a>--}}
+            </div>
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                        data-target="#header-navbar" aria-expanded="false"><span class="sr-only"></span> <span
+                            class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span>
+                </button>
+                <h1 class="logo hvr-bounce-in" style="margin-top:10px;"><a href="{{ route('index') }}"
+                                                                           title="">{{ config('app.name') }}</a></h1>
+            </div>
+            <div class="collapse navbar-collapse" id="header-navbar">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a data-cont="{{ config('app.name') }}" href="{{ route('index') }}">@lang('index.index')</a>
+                    </li>
+                    @if(isset($nav))
+                        @foreach($nav as $v)
+                            <li><a href="{{ route('category', ['id' => $v->id]) }}">{{ $v->catname }}</a></li>
+                        @endforeach
+                    @endif
+                </ul>
+            </div>
+        </div>
+    </nav>
 </header>
 <section class="container">
-	@yield('content')
+    @yield('content')
 </section>
 <footer class="footer">
-	<div class="container">
-		<p>Copyright © 2012 - {{ date('Y') }} <a
-					href="{{ route('index') }}">{{ config('app.name','LaravelManageSystem') }}</a>
-			&amp; 版权所有
-			<a href="http://www.miitbeian.gov.cn/" target="_blank">{{ base64_decode(env('BASE64_ICP')) }}</a>
-		</p>
-	</div>
-	<div id="gotop" style="display: block;"><a class="gotop" draggable="false"></a></div>
+    <div class="container">
+        <p>Copyright © 2012 - {{ date('Y') }} <a
+                    href="{{ route('index') }}">{{ config('app.name','LaravelManageSystem') }}</a>
+            &amp; 版权所有
+            <a href="http://www.miitbeian.gov.cn/" target="_blank">{{ base64_decode(env('BASE64_ICP')) }}</a>
+        </p>
+    </div>
+    <div id="gotop" style="display: block;"><a class="gotop" draggable="false"></a></div>
 </footer>
 <!-- Scripts -->
 @yield('scripts')
 <div style="display: none;">
-	<script src="https://s5.cnzz.com/z_stat.php?id=2984609&web_id=2984609" language="JavaScript"></script>
+    <script src="https://s5.cnzz.com/z_stat.php?id=2984609&web_id=2984609" language="JavaScript"></script>
 </div>
 </body>
 </html>
@@ -136,12 +137,19 @@
     $("#navbar").navSmartFloat();
     $("img.lazyload").lazyload();
     $.ajax({
-        url:'{{ route('weather') }}',
-        dataType:'json',
-        success:function(res){
+        url: '{{ route('weather') }}',
+        dataType: 'json',
+        success: function (res) {
             console.log(res);
-            if(res.status){
-
+            if (res.status) {
+                var data = res.lives[0];
+                var str = data.province + '省' + data.city + ' ' + data.weather
+                    + ' ,当前温度：' + data.temperature
+                    + '℃, 风向：' + data.winddirection
+                    + ',风力：' + data.windpower
+                    + '级,湿度:' + data.humidity
+                    + '%, 数据发布的时间' + data.reporttime;
+                $('.link-border').html(str);
             }
         }
     })
