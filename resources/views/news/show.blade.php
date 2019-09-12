428cd905c7316e819c0eee9832c7d69960c9ff15
王改朝 <wang.gaichao@163.com>
2019-09-12 16:16:19 +0800

    2019.09.12.1 畅言广告太多，删掉

--- resources/views/news/show.blade.php
+++ resources/views/news/show.blade.php
@@ -35,8 +35,6 @@
             <article class="article-content">
                 {!! $news->content or '' !!}
             </article>
-            <!--PC和WAP自适应版-->
-            <div id="SOHUCS" sid="{{ $news->id }}"></div>
 
         </div>
     </div>
@@ -65,42 +63,4 @@
             </ul>
         </div>
     </aside>
-@endsection
-
-@section('scripts')
-    <script type="text/javascript">
-        (function () {
-            var appid = 'cyqUvIxsm';
-            var conf = 'prod_b426b30042abbc15e363cb679bbc937d';
-            var width = window.innerWidth || document.documentElement.clientWidth;
-            if (width < 960) {
-                window.document.write('<script id="changyan_mobile_js" charset="utf-8" type="text/javascript" src="https://changyan.sohu.com/upload/mobile/wap-js/changyan_mobile.js?client_id=' + appid + '&conf=' + conf + '"><\/script>');
-            } else {
-                var loadJs = function (d, a) {
-                    var c = document.getElementsByTagName("head")[0] || document.head || document.documentElement;
-                    var b = document.createElement("script");
-                    b.setAttribute("type", "text/javascript");
-                    b.setAttribute("charset", "UTF-8");
-                    b.setAttribute("src", d);
-                    if (typeof a === "function") {
-                        if (window.attachEvent) {
-                            b.onreadystatechange = function () {
-                                var e = b.readyState;
-                                if (e === "loaded" || e === "complete") {
-                                    b.onreadystatechange = null;
-                                    a()
-                                }
-                            }
-                        } else {
-                            b.onload = a
-                        }
-                    }
-                    c.appendChild(b)
-                };
-                loadJs("https://changyan.sohu.com/upload/changyan.js", function () {
-                    window.changyan.api.config({appid: appid, conf: conf})
-                });
-            }
-        })();
-    </script>
 @endsection
