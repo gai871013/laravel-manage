3fc4509fd36e4398bd9014b3cc388d0581e7628b
王改朝 <wang.gaichao@163.com>
2021-03-19 16:17:29 +0800

    21.3.19 优化

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -71,7 +71,7 @@
                     </li>
                     @if(isset($nav))
                         @foreach($nav as $v)
-                            <li><a class="no_pjax" href="{{ $v->url ?? route('category', ['id' => $v->id]) }}">{{ $v->catname }}</a></li>
+                            <li><a @if($v->url) class="no_pjax" @endif href="{{ $v->url ?? route('category', ['id' => $v->id]) }}">{{ $v->catname }}</a></li>
                         @endforeach
                     @endif
                 </ul>
