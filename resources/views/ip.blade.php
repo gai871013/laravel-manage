7491b1bb9c6cb047d9d791dcfc25d40160886921
王改朝 <wang.gaichao@163.com>
2021-03-19 16:18:06 +0800

    21.3.19 优化

--- resources/views/ip.blade.php
+++ resources/views/ip.blade.php
@@ -52,6 +52,14 @@
 @endsection
 
 @section('scripts')
+    <!-- 引入样式 -->
+    <link rel="stylesheet" href="https://unpkg.com/element-ui/lib/theme-chalk/index.css">
+    <!-- import Vue before Element -->
+    <script src="https://unpkg.com/vue/dist/vue.js"></script>
+    <!-- 引入组件库 -->
+    <script src="https://unpkg.com/element-ui/lib/index.js"></script>
+    <!-- axios -->
+    <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
     <script>
         new Vue({
             el: '#app',
