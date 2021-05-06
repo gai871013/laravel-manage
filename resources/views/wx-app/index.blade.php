0d615e99f196f009b9344d51b576dd9f8ea4cd8a
王改朝 <wang.gaichao@163.com>
2021-05-06 15:41:01 +0800

    21.5.6

--- resources/views/wx-app/index.blade.php
+++ resources/views/wx-app/index.blade.php
@@ -6,12 +6,15 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>测试小程序</title>
+    <script src="https://res.wx.qq.com/open/js/jweixin-1.3.2.js"></script>
 </head>
 <body>
+<a href="javascript:ready()">点击测试</a>
 <script>
     // web-view下的页面内
     function ready() {
         console.log(window.__wxjs_environment === 'miniprogram') // true
+        alert(window.__wxjs_environment);
     }
 
     if (!window.WeixinJSBridge || !WeixinJSBridge.invoke) {
@@ -19,6 +22,11 @@
     } else {
         ready()
     }
+
+    // 或者
+    wx.miniProgram.getEnv(function (res) {
+        console.log(res.miniprogram) // true
+    })
 </script>
 </body>
 </html>
