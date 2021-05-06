c4b2f1cfa2ee9d1dc348194ad7df5c0ebecdd294
王改朝 <wang.gaichao@163.com>
2021-05-06 16:34:21 +0800

    21.5.6 小程序支付

--- resources/views/wx-app/index.blade.php
+++ resources/views/wx-app/index.blade.php
@@ -6,6 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>测试小程序</title>
+    <script src="https://res.wx.qq.com/open/js/jweixin-1.3.2.js"></script>
 </head>
 <body>
 <a href="javascript:ready()">点击测试</a>
@@ -14,10 +15,8 @@
     function ready() {
         console.log(window.__wxjs_environment === 'miniprogram') // true
         if (window.__wxjs_environment === 'miniprogram') {
-            loadJs('https://res.wx.qq.com/open/js/jweixin-1.3.2.js').then(() => {
-                wx.miniProgram.redirectTo({
-                    url:'/pages/wxpay/wxpay?appId=wx940ea58d8789b8ff&timeStamp=1620276669&nonceStr=609375bdd146c&package=prepay_id=wx061251097921020f6c4370880acd380000&signType=MD5&paySign=BF5A7E664DF6428A6E6F78896C469EE1&order_no=210506125109000025'
-                })
+            wx.miniProgram.redirectTo({
+                url:'/pages/wxpay/wxpay?appId=wx940ea58d8789b8ff&timeStamp=1620276669&nonceStr=609375bdd146c&package=prepay_id=wx061251097921020f6c4370880acd380000&signType=MD5&paySign=BF5A7E664DF6428A6E6F78896C469EE1&order_no=210506125109000025'
             })
         }
     }
