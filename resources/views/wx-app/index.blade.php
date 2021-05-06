dbb7ac7478bc9303b94787a1b2da6525de17aa83
王改朝 <wang.gaichao@163.com>
2021-05-06 16:30:14 +0800

    21.5.6 小程序支付

--- resources/views/wx-app/index.blade.php
+++ resources/views/wx-app/index.blade.php
@@ -6,7 +6,6 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>测试小程序</title>
-    <script src="https://res.wx.qq.com/open/js/jweixin-1.3.2.js"></script>
 </head>
 <body>
 <a href="javascript:ready()">点击测试</a>
@@ -14,7 +13,13 @@
     // web-view下的页面内
     function ready() {
         console.log(window.__wxjs_environment === 'miniprogram') // true
-        alert(window.__wxjs_environment);
+        if (window.__wxjs_environment === 'miniprogram') {
+            loadJs('https://res.wx.qq.com/open/js/jweixin-1.3.2.js').then(() => {
+                wx.miniProgram.redirectTo({
+                    url:'/pages/wxpay/wxpay?appId=wx940ea58d8789b8ff&timeStamp=1620276669&nonceStr=609375bdd146c&package=prepay_id=wx061251097921020f6c4370880acd380000&signType=MD5&paySign=BF5A7E664DF6428A6E6F78896C469EE1&order_no=210506125109000025'
+                })
+            })
+        }
     }
 
     if (!window.WeixinJSBridge || !WeixinJSBridge.invoke) {
@@ -23,10 +28,20 @@
         ready()
     }
 
-    // 或者
-    wx.miniProgram.getEnv(function (res) {
-        console.log(res.miniprogram) // true
-    })
+    function loadJs(src) {
+        return new Promise((resolve, reject) => {
+            let script = document.createElement('script')
+            script.type = "text/javascript"
+            script.onload = () => {
+                resolve()
+            }
+            script.onerror = () => {
+                reject()
+            }
+            script.src = src
+            document.getElementsByTagName('body')[0].appendChild(script)
+        })
+    }
 </script>
 </body>
 </html>
