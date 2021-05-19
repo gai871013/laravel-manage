7892f161bf592269567950a95f7822a06740acc3
王改朝 <wang.gaichao@163.com>
2021-05-19 14:57:17 +0800

    ip

--- app/Http/Controllers/IpController.php
+++ app/Http/Controllers/IpController.php
@@ -28,8 +28,9 @@ class IpController extends Controller
     {
         $ip2region = new Ip2Region();
         $ip        = request()->input('ip') ?? request()->ip();
+        $res       = IpLocation::getLocation($ip);
         try {
-            return ['code' => 0, 'msg' => '获取成功', 'data' => IpLocation::getLocation($ip), 'ipip' => (new City())->find($ip), 'ip2region' => $ip2region->btreeSearch($ip)];
+            return ['code' => 0, 'msg' => '获取成功', 'data' => $res, 'ipip' => (new City())->find($ip), 'ip2region' => $ip2region->btreeSearch($res['ip'])];
         } catch (Exception $e) {
             return ['code' => 1, 'msg' => $e->getMessage()];
         }
