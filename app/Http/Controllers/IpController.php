5864c79324c7c9cfd1a441330c400175d630602a
王改朝 <wang.gaichao@163.com>
2021-05-19 15:01:45 +0800

    debug

--- app/Http/Controllers/IpController.php
+++ app/Http/Controllers/IpController.php
@@ -29,8 +29,10 @@ class IpController extends Controller
         $ip2region = new Ip2Region();
         $ip        = request()->input('ip') ?? request()->ip();
         try {
-            $res       = IpLocation::getLocation($ip);
-            return ['code' => 0, 'msg' => '获取成功', 'data' => $res, 'ipip' => (new City())->find($ip), 'ip2region' => $ip2region->btreeSearch($res['ip'])];
+            $res  = IpLocation::getLocation($ip);
+            $res2 = $ip2region->btreeSearch($res['ip']);
+            dd($ip, $res, $res2, $ip2region->btreeSearch($ip));
+            return ['code' => 0, 'msg' => '获取成功', 'data' => $res, 'ipip' => (new City())->find($ip), 'ip2region' => $res2];
         } catch (Exception $e) {
             return ['code' => 1, 'msg' => $e->getMessage()];
         }
