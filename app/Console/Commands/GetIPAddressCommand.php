020019aef561b4859817eca2c63c06c74fe8edf8
王改朝 <wang.gaichao@163.com>
2022-06-22 11:08:59 +0800

    优化

--- app/Console/Commands/GetIPAddressCommand.php
+++ app/Console/Commands/GetIPAddressCommand.php
@@ -40,16 +40,18 @@ class GetIPAddressCommand extends Command
 //        $res2 = preg_match_all($reg2, $ip, $ips2, PREG_PATTERN_ORDER);
         // PREG_SET_ORDER
         $all  = [];
+        $res_ip = [];
         $file = storage_path('logs/ip_result_' . date('YmdHis') . '.log');
         foreach ($ips[0] as $v) {
-            if (in_array($v, $all)) {
+            if (in_array($v, $res_ip)) {
                 continue;
             }
+            $res_ip[] = $v;
             $ip_add = app('IpLocation')->getLocation($v);
             $all[]  = $result = '[' . $v . ']:' . $ip_add['country'] . ' ' . $ip_add['area'];
             file_put_contents($file, $result . PHP_EOL, FILE_APPEND);
         }
         info($path . ' 解析结果', $all);
-        $this->info('共匹配' . $res . '条，共解析' . count($all) . '条，解析结果请查看log文件');
+        $this->info('共匹配' . $res . '条，共过滤并解析' . count($all) . '条，解析结果请查看log文件');
     }
 }
